c66743b55293777337e1b882845cbac3c772f5ba
weicheng.zhu(travis) <weicheng.zhu@rakuten.com>
2018-09-20 15:52:12 +0800

    application development

--- tensorflow/nlp/app.py
+++ tensorflow/nlp/app.py
@@ -16,9 +16,11 @@ def create_app(debug=False):
 
 app = create_app(debug=True)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/location/extract/<sentence>')
 def location_extract(sentence):
@@ -27,9 +29,46 @@ def location_extract(sentence):
 
     predictions = predictor.predict(sentences_texts)
 
-    trie.pos_tag(sentence)
+    pos_tags = trie.pos_tag(sentence)
 
-    json_result = json.dumps({'data': predictions[0], 'success': True})
+    # Hybrid approach for NER
+    concept_objects = list()
+    hypothesis_pos_tags = list()
+    for pos_tag in pos_tags:
+        word = pos_tag[0]
+        tags = pos_tag[1]
+
+        # Decide if it's a determined word (is in the dictionary and not ambiguous), confidence 1.0
+        if len(tags) == 1:
+            concept_object = {'value': word, 'class': tags[0], 'confidence': 1.0}
+            concept_objects.append(concept_object)
+        elif len(tags) > 1:
+            hypothesis_pos_tags.append(pos_tag)
+
+    for prediction in predictions[0]:
+        pred_word = prediction[0]
+        pred_tag = prediction[1]
+
+        # Decide if it's a discovered word (not in the dictionary), confidence 0.7
+        is_discover_word = True
+        for concept_object in concept_objects:
+            if pred_word == concept_object['value'] and pred_tag == concept_object['class']:
+                is_discover_word = False
+
+        if is_discover_word:
+            concept_object = {'value': pred_word, 'class': pred_tag, 'confidence': 0.7}
+            concept_objects.append(concept_object)
+
+        # Decide if it's an ambiguous word (in the dictionary but with multiple tags), confidence 0.9
+        for hypothesis_pos_tag in hypothesis_pos_tags:
+            word = hypothesis_pos_tag[0]
+            tags = hypothesis_pos_tag[1]
+
+            if pred_word == word and pred_tag in tags:
+                concept_object = {'value': pred_word, 'class': pred_tag, 'confidence': 0.9}
+                concept_objects.append(concept_object)
+
+    json_result = json.dumps({'data': concept_objects, 'success': True})
     resp = Response(json_result, status=200, mimetype='application/json')
 
     return resp
